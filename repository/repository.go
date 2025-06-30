81d412206c44ead0085da7ffc7b00f9835a12c6b
Pyae Soe Kyaw <pyaesoekyaw1123@gmail.com>
2025-06-30 23:44:14 +0630

    Update repository.go

--- repository/repository.go
+++ repository/repository.go
@@ -50,9 +50,12 @@ func NewRepository() (*Repository, error) {
 // CreateUser saves a new user to the database
 func (r *Repository) CreateUser(user *model.User) error {
 	result := r.DB.Create(user)
-	if result.Error != nil {
-		return fmt.Errorf("failed to create user: %w", result.Error)
-	}
+	if err.Error() == "failed to create user: ERROR: duplicate key value violates unique constraint \"users_username_key\" (SQLSTATE 23505)" {
+    http.Error(w, "Username already exists", http.StatusConflict) // This should be sent
+} else {
+    log.Printf("Error creating user: %v", err)
+    http.Error(w, "Error creating user", http.StatusInternalServerError) // This is what you're seeing
+}
 	return nil
 }
 
